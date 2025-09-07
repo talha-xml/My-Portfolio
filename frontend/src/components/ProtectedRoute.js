91a81048040d1b77c0d6ff2d0863c0f5a4331bb5
Talha0103 <mtalhafaizan30@gmail.com>
2025-09-08 01:25:16 +0500

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -4,12 +4,10 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
 
-  // ✅ if no token, redirect to login
   if (!token) {
     return <Navigate to="/login" replace />;
   }
 
-  return children; // ✅ if logged in, render the child component
+  return children; 
 }
-
 export default ProtectedRoute;
