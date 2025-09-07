d8d90874d9c9d1f251818c6a9bde94c2c44fa800
Talha0103 <mtalhafaizan30@gmail.com>
2025-09-08 01:15:46 +0500

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -5,7 +5,6 @@ from flask_jwt_extended import JWTManager
 from config import Config
 from Auth_DB_LOGIC.db import db
 
-# Blueprints
 from Auth_DB_LOGIC.signup_logic import signup_bp
 from Auth_DB_LOGIC.login_logic import login_bp
 from Auth_DB_LOGIC.contact_logic import contact_bp
@@ -16,17 +15,14 @@ from services.currency_service import currency_bp
 def create_app():
     app = Flask(__name__)
 
-    # Configurations
     app.config["SQLALCHEMY_DATABASE_URI"] = Config.DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = Config.JWT_SECRET_KEY
 
-    # Extensions
     db.init_app(app)
     CORS(app)
     JWTManager(app)
 
-    # Register blueprints
     app.register_blueprint(signup_bp, url_prefix="/api")
     app.register_blueprint(login_bp, url_prefix="/api")
     app.register_blueprint(contact_bp, url_prefix="/api")
@@ -34,7 +30,6 @@ def create_app():
     app.register_blueprint(currency_bp, url_prefix="/api")
 
     return app
-
 
 app = create_app()
 
